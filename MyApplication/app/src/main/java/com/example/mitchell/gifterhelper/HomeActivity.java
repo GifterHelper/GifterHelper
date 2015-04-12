2ff9ecc3ef0adef5f2b0c7d211c499f41663e5fd
Ethan Ito <ethanitorhs@gmail.com>
2015-04-12 14:05:46 -0700

    Added skeleton activity class for home page, put logging details for registration page as well as having registration activity load the main activity after successful registration

---
+++ MyApplication/app/src/main/java/com/example/mitchell/gifterhelper/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.mitchell.gifterhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ethan on 4/12/2015.
+ */
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.home_layout);
+    }
+}
--- MyApplication/app/src/main/java/com/example/mitchell/gifterhelper/RegisterActivity.java
+++ MyApplication/app/src/main/java/com/example/mitchell/gifterhelper/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.example.mitchell.gifterhelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 import android.widget.Toast;
 
 /**
@@ -24,24 +27,37 @@ public class RegisterActivity extends Activity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Check to see if username and password fields are filled
+                boolean notfilled = false;
                 if(username.getText().length() == 0){
                     Toast.makeText(RegisterActivity.this,"Please enter username",Toast.LENGTH_SHORT).show();
+                    Log.e("GifterHelper","Missing username");
+                    notfilled = true;
                 }
                 if(password.getText().length() == 0 || matchpassword.getText().length() == 0){
                     Toast.makeText(RegisterActivity.this,"Please enter password",Toast.LENGTH_SHORT).show();
+                    Log.e("GifterHelper","Missing password");
+                    notfilled = true;
                 }
-                if(password.getText().toString().equals(matchpassword.getText().toString())){
+                if(!notfilled & password.getText().toString().equals(matchpassword.getText().toString())){
                     //check to see if username has been used
 
                     boolean validUserName = true;
-                    if(validUserName){
-                        //Load main menu
-
+                    if(validUserName) {
+                        //Load login
+                        Log.i("GifterHelper", "Valid Username");
+                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                        RegisterActivity.this.startActivity(intent);
+                    }else{
+                        Toast.makeText(RegisterActivity.this,"Username taken",Toast.LENGTH_SHORT).show();
+                        Log.e("GifterHelper","Username Taken");
                     }
                 }else{
                     Toast.makeText(RegisterActivity.this,"Passwords do not match",Toast.LENGTH_SHORT).show();
                     //Clear confirmation password
+                    Log.e("GifterHelper","Incorrect Passowrd");
                     matchpassword.setText("");
+                    Log.d("GifterHelper","Cleared match password edittext field");
                 }
             }
         });
