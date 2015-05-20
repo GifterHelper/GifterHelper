063e97dae7c929e623bdfacddeb05d725e10c34a
Ethan Ito <ethanitorhs@gmail.com>
2015-05-19 18:56:57 -0700

    Fixed login multiple times with saved user

--- MyApplication/app/src/main/java/com/gifterhelper/MainActivity.java
+++ MyApplication/app/src/main/java/com/gifterhelper/MainActivity.java
@@ -50,45 +50,27 @@ public class MainActivity extends Activity {
                     notfilled = true;
                 }
                 if (!notfilled) {
-                    //Backend check for user/pass
+                    //We first check if we have the user in storage
                     ParseQuery<User> userParseQuery = new ParseQuery<User>(User.class);
                     userParseQuery.whereContains("username", username.getText().toString());
-                    Log.d("GifterHelper", "Query for user " + username.getText().toString());
+                    Log.d("GifterHelper", "Query for user " + username.getText().toString() + "locally");
+                    userParseQuery.fromLocalDatastore();
                     userParseQuery.findInBackground(new FindCallback<User>() {
                         @Override
                         public void done(List<User> users, ParseException e) {
                             if (users.size() > 1) {
                                 Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
                                 Log.d("GifterHelper", "Retrieved more than one user");
-                            } else if (users.size() == 0) {
-                                Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
-                                Log.d("GifterHelper", "Invalid username");
+                            } else if (users.size() == 1) {
+                                User user = users.get(0);
+                                id = user.getId();
+                                Toast.makeText(MainActivity.this, "Username/Password valid", Toast.LENGTH_SHORT).show();
+                                Log.d("GifterHelper", "Loading from local datastore");
+                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                intent.putExtra("id", id);
+                                MainActivity.this.startActivity(intent);
                             } else {
-                                if (password.getText().toString().equals(users.get(0).getPassword())) {
-                                    //Get user id from backend
-                                    User user = users.get(0);
-                                    id = user.getId();
-                                    //String userName = users.get(0).getUserName();
-                                    //String userPass = users.get(0).getPassword();
-                                    /*Log.d("GifterHelper", "Id is : " + id);
-                                    Log.d("GifterHelper", "Name is : " + userName);
-                                    Log.d("GifterHelper", "Password is : " + userPass);*/
-                                    //Load homepage
-                                    Log.i("GifterHelper", "Valid Username & Password login");
-                                    //We create a new user to store the values that is necessary
-                                    //TODO find another way of storing friends, possibly new parse object
-                                    User userSave = new User();
-                                    userSave.saveDetail(user.getId(),user.getFriends(),user.getWishlist(),user.getHistory());
-                                    //user.saveInBackground();
-                                    user.pinInBackground();
-                                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                                    intent.putExtra("id", id);
-                                    MainActivity.this.startActivity(intent);
-                                } else {
-                                    Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
-                                    Log.e("GifterHelper", "Password Incorrect");
-                                }
-
+                                BackgroundQuery(username.getText().toString(),password.getText().toString());
                             }
                         }
                     });
@@ -106,5 +88,52 @@ public class MainActivity extends Activity {
                 MainActivity.this.startActivity(intent);
             }
         });
+    }
+
+    void BackgroundQuery(String username, String password){
+        //Backend check for user/pass
+        final String pass = password;
+        ParseQuery<User> remoteUserParseQuery = new ParseQuery<User>(User.class);
+        remoteUserParseQuery.whereContains("username", username);
+        Log.d("GifterHelper", "Query for user " + username);
+        remoteUserParseQuery.findInBackground(new FindCallback<User>() {
+            @Override
+            public void done(List<User> users, ParseException e) {
+                if (users.size() > 1) {
+                    Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
+                    Log.d("GifterHelper", "Retrieved more than one user");
+                } else if (users.size() == 0) {
+                    Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
+                    Log.d("GifterHelper", "Invalid username");
+                } else {
+                    if (pass.equals(users.get(0).getPassword())) {
+                        //Get user id from backend
+                        User user = users.get(0);
+                        id = user.getId();
+                        //String userName = users.get(0).getUserName();
+                        //String userPass = users.get(0).getPassword();
+                                                /*Log.d("GifterHelper", "Id is : " + id);
+                                                Log.d("GifterHelper", "Name is : " + userName);
+                                                Log.d("GifterHelper", "Password is : " + userPass);*/
+                        //Load homepage
+                        Log.i("GifterHelper", "Valid Username & Password login");
+                        //We create a new user to store the values that is necessary
+                        //TODO find another way of storing friends, possibly new parse object
+                        User userSave = new User();
+                        userSave.saveDetail(user.getId(),user.getFriends(),user.getWishlist(),user.getHistory());
+                        //user.saveInBackground();
+                        user.pinInBackground();
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        intent.putExtra("id", id);
+                        MainActivity.this.startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Username/Password not valid", Toast.LENGTH_SHORT).show();
+                        Log.e("GifterHelper", "Password Incorrect");
+                    }
+
+                }
+            }
+        });
+
     }
 }
